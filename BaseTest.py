e2fdb24b7cf897b51a2e549f9b27298779ae1b30
Austin Hamilton <Austin Hamilton>
2016-12-18 00:31:59 -0600

    Adding some .gitignore stuff and updating main script

--- BaseTest.py
+++ BaseTest.py
@@ -7,22 +7,46 @@
 
 import os
 import argparse
-
+import glob
+import platform
 
 ###########
 # GLOBALS #
 ###########
 
-# List of test scripts for the CPUEmulator.
-tests = {
-	
-}
+oper_sys = platform.platform()
+
+windows = 'windows' in oper_sys.lower()
+
 # Current directory.
 cwd = os.path.dirname(os.path.realpath(__file__))
-# Paths to virtual machine emulator and cpu emulator, respectively.
-# Obviously, this path will be different on different machines.
-vm_emulator = ''
+
+print('Hello');
+
+hdl_files   = [file for file in glob.iglob(cwd + '/**/projects/**/*.hdl', recursive=True)]
+tst_files   = [file for file in glob.iglob(cwd + '/**/projects/**/*.tst', recursive=True)]
+batch_files = [file for file in glob.iglob(cwd + '/**/tools/*.bat')]
+sh_files    = [file for file in glob.iglob(cwd + '/**/tools/*.sh')]
+
+vm_emulator  = ''
 cpu_emulator = ''
+assembler    = ''
+hardware_sim = ''
+jack_cmpiler = ''
+
+for f in tst_files:
+	if('VM' in f):
+		vm_emulator = f
+	else if('CPU' in f):
+		cpu_emulator = f
+	else if('Assembler' in f):
+		assembler = f
+	else if('Hardware' in f):
+		hardware_sim = f
+	else if('Jack' in f):
+		jack_cmpiler = f
+
+
 
 
 ########################
@@ -31,6 +55,7 @@ cpu_emulator = ''
 
 # Configure command line options.
 # http://stackoverflow.com/a/7427376/5415895
+
 parser = argparse.ArgumentParser(description='Test the virtual machine.')
 parser.add_argument('-f','--filename', help='The file to translate. If none is given, every file in the tests list will be tested.', required=False)
 parser.add_argument('-v','--vm_dir', help='Directory for vm emulator.', required=False)
@@ -42,20 +67,20 @@ args = vars(parser.parse_args())
 # PARSE ARGUMENTS #
 ###################
 
-file = args['filename']
+# file = args['filename']
 
-if args['vm_dir']:
-	vm_emulator = args['vm_dir']
-if args['cpu_dir']:
-	cpu_emulator = args['cpu_dir']
+# if args['vm_dir']:
+# 	vm_emulator = args['vm_dir']
+# if args['cpu_dir']:
+# 	cpu_emulator = args['cpu_dir']
 
-if args['filename']:
-	# Run test scripts.
-	else:
-		print("Testing " + file + " with the CPUEmulator.")
-		os.system("bash " + cpu_emulator + " " + file)
-else:
-	for test in tests:
-		print("Testing " + test + " with the CPUEmulator.")
-		os.system('bash ' + cpu_emulator + " " + test)
-		print("\n")
+# if args['filename']:
+# 	# Run test scripts.
+# 	else:
+# 		print("Testing " + file + " with the CPUEmulator.")
+# 		os.system("bash " + cpu_emulator + " " + file)
+# else:
+# 	for test in tests:
+# 		print("Testing " + test + " with the CPUEmulator.")
+# 		os.system('bash ' + cpu_emulator + " " + test)
+# 		print("\n")
